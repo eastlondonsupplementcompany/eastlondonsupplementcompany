2fbdefec04e5234073355db5126867b15e91837e
hrowett <henry.rowett@gmail.com>
2020-01-28 22:22:19 +0000

    fixing padding for mobile

--- src/views/index-sections/AboutUs.js
+++ src/views/index-sections/AboutUs.js
@@ -27,13 +27,13 @@ class AboutUs extends React.Component {
                   </Col>
                 <Col md="12">
                   <h2>About Us</h2>
-                  <p>
+                  <p className="">
                         We are East London Supplement Company. We provide organic, vegan, GMO-free protein supplements to our customers at affordable prices.
                         Our goal is to improve the lives of our customers through nutrion and reduce the impact of mainstream supplement suppliers on the environment.
                         We are currently at conception stage for our first plant based protein supplement. We have agreed a supplier and now begin the process of optimising
                         a blend of protein rich plant based ingredients to create a nutrtional, environmentally friendly protein supplement suitable for building, maintaining 
                         and supporting the body.
-                      </p>
+                  </p>
                 </Col>
               </Row>
             </Container>
